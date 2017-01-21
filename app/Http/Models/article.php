7ae7b51778780b8ad6d65eb1a05e7c237871502e
e.Solomein <eosolomein@gmail.com>
2017-01-21 21:20:25 +0500

    Add Laravel-Debugbar and Laravel-ide-helper

--- app/Http/Models/article.php
+++ app/Http/Models/article.php
@@ -6,6 +6,11 @@ use DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Http\Models\Article
+ *
+ * @mixin \Eloquent
+ */
 class Article extends Model
 {
     //
