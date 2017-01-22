31d09c3cf22659a525c2ca76bf2e35ead4e24ba7
e.Solomein <eosolomein@gmail.com>
2017-01-22 13:22:58 +0500

    change 404 image

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,5 +4,5 @@
     <h1>404 Not Found</h1>
     <h3>К сожалению, такая страница не найдена</h3>
     <a href="/">Перейдите на главную</a>
-    <p><img src="https://psv4.vk.me/c812537/u10699527/docs/19aae20dcd8f/gifki-kote-granitsa-3538938.gif" alt=""></p>
+    <p><img src="/img/404.jpg" alt=""></p>
 @endsection
